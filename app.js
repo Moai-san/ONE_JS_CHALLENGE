fc18691569828211923f3afa9d821dae550091a2
Leonardo Gonzalez Catalan <70494457+Moai-san@users.noreply.github.com>
2025-02-08 06:07:09 -0300

    Backend
    Basic js, first version, need to update it so it modifies frontend.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// LEONARDO GONZALEZ CATALAN [MOAI-SAN] - FEBRUARY 08, 2025. ORACLE ONE PROGRAM, GROUP 8.
+
+//GLOBAL VARS
+
+//secret friend array
+var secretFriend = new Array();
+
+/* FUNCTIONS */
+
+//Random int generator between two values
+function randInt(min = 0, max = secretFriend.length)
+{
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+//puts friend into "secret friend" array
+function pushFriend(friend)
+{
+    secretFriend.push(friend);
+}
+
+//gets random friend from the array
+function getRandom_friend()
+{
+    return(secretFriend[ randInt(0,secretFriend.length) ])
+}
+
+/* MAIN BODY*/
+
+
